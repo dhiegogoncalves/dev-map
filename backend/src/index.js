48f658c6fcb885ac0465205e746bf8fcb0899a46
Dhiego <dhhiego@gmail.com>
2020-03-21 23:00:46 -0400

    implementação do frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes.js');
 
 const app = express();
 
-mongoose.connect(
-  'mongodb+srv://admin:dev-map@cluster0-jj1ch.mongodb.net/dev-map?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.APP_PORT);
